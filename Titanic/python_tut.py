e878c0901eb5d0c50967a302910569ae26a5a42e
vaastav <vaastav.anand05@gmail.com>
2017-07-10 23:29:44 -0700

    Add Titanic data and tutorial script

---
+++ Titanic/python_tut.py
@@ -0,0 +1,24 @@
+import csv as csv
+import numpy as np
+
+csv_file = csv.reader(open('train.csv','rb'))
+header = csv_file.next()
+
+data = []
+
+for row in csv_file:
+	data.append(row)
+data = np.array(data)
+number_passengers = np.size(data[0::,1].astype(np.float))
+number_survived = np.sum(data[0::,1].astype(np.float))
+proportion_survivors = number_survived/number_passengers
+women_data = data[0::,4] == "female"
+men_data = data[0::,4]!= "female"
+
+women_onboard = data[women_data,1].astype(np.float)
+men_onboard = data[men_data,1].astype(np.float)
+proportion_women_survived = np.sum(women_onboard)/np.size(women_onboard)
+proportion_men_survived = np.sum(men_onboard)/np.size(men_onboard)
+
+print('Proportion of women who survied is %s' % proportion_women_survived)
+print('Proportion of men who survived is %s' % proportion_men_survived)
